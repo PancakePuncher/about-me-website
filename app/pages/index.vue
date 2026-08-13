b571663f5c889acd2932a1f4c9cb793e7a9da1b9
pancakepuncher <pancakepuncher@gmail.com>
2026-08-13 02:06:19 +0000

    Add more info to page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const value = ref(50);
+const value = ref(66);
 </script>
 
 <template>
@@ -10,13 +10,23 @@ const value = ref(50);
       class="h-48 w-48 rounded-full object-cover aspect-square"
     />
     <div class="flex flex-col items-center w-full pt-15 gap-4">
-      <h1>Western Governers University</h1>
-      <h2>B.S. Cybersecurity and Data Assurance</h2>
-      <UProgress v-model="value" class="w-[35%]" size="lg" />
+      <h1 class="bg-blue-600 pt-2 pb-2 pl-5 pr-5 rounded-full text-3xl">About Me</h1>
+      <p class="text-center w-[75%]">Hello! I am Alex (a.k.a PancakePuncher). 
+        I enjoy coding, system administration, and generally all things technology. 
+        I made this page to showcase my work and track my educational accomplishments. 
+        Below you can get a glimpse of my current education and certifications.
+      </p>
+    </div>
+    <div class="flex flex-col items-center w-full pt-15 gap-4">
+      <h1 class="bg-blue-600 pt-2 pb-2 pl-5 pr-5 rounded-full text-3xl">Education</h1>
+        <h1>Western Governers University</h1>
+        <h2>B.S. Cybersecurity and Data Assurance</h2>
+        <UProgress v-model="value" class="w-[50%]" size="lg" />
+        <h2>Est. Graduation by 2027</h2>
     </div>
     <div class="flex flex-col items-center pt-15 gap-3.5">
-      <h1>CompTIA Certifications</h1>
-      <div class="flex flex-row flex-wrap gap-5 pt-2">
+      <h1 class="bg-blue-600 pt-2 pb-2 pl-5 pr-5 rounded-full text-3xl text-center">Current/Active Certifications</h1>
+      <div class="flex flex-col md:flex-row flex-wrap gap-5 pt-2">
         <img
           src="/certlogos/aplus.png"
           alt=""
@@ -32,28 +42,10 @@ const value = ref(50);
           alt=""
           class="h-36 w-36 rounded-full object-cover aspect-square"
         />
-      </div>
-      <h1>Other Certifications</h1>
-      <div class="flex flex-row flex-wrap gap-5 pt-2">
+        <img src="/certlogos/lec.png" alt="" class="h-36 w-36" />
         <img src="/certlogos/itil4.png" alt="" class="h-36 w-36" />
-      </div>
-      <h1>Planned</h1>
-      <div class="flex flex-row flex-wrap gap-5 pt-2">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/62/Solid_red.svg"
-          alt=""
-          class="h-36 w-36 rounded-full object-cover aspect-square opacity-35"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/62/Solid_red.svg"
-          alt=""
-          class="h-36 w-36 rounded-full object-cover aspect-square opacity-35"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/62/Solid_red.svg"
-          alt=""
-          class="h-36 w-36 rounded-full object-cover aspect-square opacity-35"
-        />
+        <img src="/certlogos/awsccp.png" alt="" class="h-36 w-36" />
+        <img src="/certlogos/googledata.png" alt="" class="h-36 w-36" />
       </div>
     </div>
   </div>
