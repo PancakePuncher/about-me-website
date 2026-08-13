81f1a94c77c8efd7a6acdbaf28be1d517855fbbd
pancakepuncher <pancakepuncher@gmail.com>
2026-08-13 02:06:06 +0000

    Add footer enhancements

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="flex flex-row justify-between opacity-25">
-    <div>Made by: PancakePuncher</div>
+  <div class="grid grid-cols-3 justify-between opacity-25 text-xs md:text-base text-center">
+    <div><a target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/pancakepuncher">Made by: PancakePuncher</a></div>
+    <div>No 🤖 used.</div>
     <div>
       &#9825;
       <a
