0a315fd01f8de51afb5cdc78d149a8944fbf42dd
pancakepuncher <pancakepuncher@gmail.com>
2026-08-14 02:47:27 +0000

    Update personal url to go directly to repo

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="grid grid-cols-3 justify-between opacity-25 text-xs md:text-base text-center">
-    <div><a target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/pancakepuncher">Made by: PancakePuncher</a></div>
+    <div><a target="_blank" rel="noopener noreferrer" href="https://github.com/pancakepuncher/about-me-website">Made by:
+        PancakePuncher</a></div>
     <div>No 🤖 used.</div>
     <div>
       &#9825;
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/deniselupe"
-        >deniselupe</a
-      >
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/deniselupe">deniselupe</a>
     </div>
   </div>
 </template>
